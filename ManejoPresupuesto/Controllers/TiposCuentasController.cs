e6ea624a0c4b0e8a0ece6b468e5cc1fbe1e73cea
Valeria Soledad Heredia <valeheredia97@gmail.com>
2023-01-02 14:06:43 -0300

    Editar tipo cuentas

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -7,15 +7,17 @@ namespace ManejoPresupuesto.Controllers
     public class TiposCuentasController : Controller
     {
         private readonly IRepositorioTiposCuentas _repositorioTiposCuentas;
+        private readonly IServicioUsuarios _servicioUsuarios;
 
-        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
         {
             _repositorioTiposCuentas = repositorioTiposCuentas;
+            _servicioUsuarios = servicioUsuarios;
         }
 
         public async Task<IActionResult> Index()
         {
-            var usuarioId = 1;
+            var usuarioId = _servicioUsuarios.ObtenerUsuarioId();
             var tiposCuentas = await _repositorioTiposCuentas.Obtener(usuarioId);
             return View(tiposCuentas);
         }
@@ -32,7 +34,7 @@ namespace ManejoPresupuesto.Controllers
             {
                 return View(tipoCuenta);
             }
-            tipoCuenta.UsuarioId = 1;
+            tipoCuenta.UsuarioId = _servicioUsuarios.ObtenerUsuarioId();
             var yaExisteTipoCuenta = 
                 await _repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
 
@@ -48,9 +50,36 @@ namespace ManejoPresupuesto.Controllers
         }
 
         [HttpGet]
+        public async Task<ActionResult> Editar(int id)
+        {
+            var usuarioId = _servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await _repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if(tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Editar(TipoCuenta tipoCuenta)
+        {
+            var usuarioId = _servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuentaExiste = await _repositorioTiposCuentas.ObtenerPorId(tipoCuenta.Id, usuarioId);
+
+            if(tipoCuentaExiste is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            await _repositorioTiposCuentas.Actualizar(tipoCuenta);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
         public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
         {
-            var usuarioId = 1;
+            var usuarioId = _servicioUsuarios.ObtenerUsuarioId();
             var yaExisteTipoCuenta = await _repositorioTiposCuentas.Existe(nombre, usuarioId);
 
             if (yaExisteTipoCuenta)
--- ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -6,9 +6,11 @@ namespace ManejoPresupuesto.Servicios
 {
     public interface IRepositorioTiposCuentas
     {
+        Task Actualizar(TipoCuenta tipoCuenta);
         Task Crear(TipoCuenta tipoCuenta);
         Task<bool> Existe(string nombre, int usuarioId);
         Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
+        Task<TipoCuenta> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioTiposCuentas: IRepositorioTiposCuentas
     {
@@ -48,6 +50,26 @@ namespace ManejoPresupuesto.Servicios
             return await connection.QueryAsync<TipoCuenta>(@"select Id, Nombre, Orden
                                                             from TiposCuentas
                                                             where UsuarioId =@UsuarioId", new {usuarioId});
+        }
+
+        public async Task Actualizar(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"update TiposCuentas
+                                        set Nombre= @Nombre
+                                        where Id = @Id", tipoCuenta);
+        }
+
+        public async Task<TipoCuenta> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<TipoCuenta>(@"select Id, Nombre, Orden
+                                                                    from TiposCuentas
+                                                                    where Id = @Id and UsuarioId =@UsuarioId", new
+            {
+                id,
+                usuarioId
+            });
         }
     }
 }
