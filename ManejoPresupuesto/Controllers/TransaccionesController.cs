9e0382597e66de58d3073942d45d34b9e0c0c3f5
Valeria Soledad Heredia <valeheredia97@gmail.com>
2023-01-20 17:11:00 -0300

    Exportar excel por año

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -171,6 +171,40 @@ namespace ManejoPresupuesto.Controllers
             return GenerarExcel(nombreArchivo, transacciones);
         }
 
+        [HttpGet]
+        public async Task <FileResult> ExportarExcelPorAño (int año)
+        {
+            var fechaInicio = new DateTime(año, 1, 1);
+            var fechaFin = fechaInicio.AddYears(1).AddDays(-1);
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var transacciones = await repositorioTransacciones.ObtenerPorUsuarioId(
+               new ParametroObtenerTransaccionesPorUsuario
+               {
+                   UsuarioId = usuarioId,
+                   FechaInicio = fechaInicio,
+                   FechaFin = fechaFin
+               });
+            var nombreArchivo = $"Manejo Presupuesto - {fechaInicio.ToString("yyyy")}.xlsx";
+            return GenerarExcel(nombreArchivo, transacciones);
+        }
+
+        [HttpGet]
+        public async Task<FileResult> ExportarExcelTodo()
+        {
+            var fechaInicio =  DateTime.Today.AddYears(-100);
+            var fechaFin = DateTime.Today.AddYears(1000);
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var transacciones = await repositorioTransacciones.ObtenerPorUsuarioId(
+               new ParametroObtenerTransaccionesPorUsuario
+               {
+                   UsuarioId = usuarioId,
+                   FechaInicio = fechaInicio,
+                   FechaFin = fechaFin
+               });
+            var nombreArchivo = $"Manejo Presupuestos - {DateTime.Today.ToString("dd-MM-yyyy")}.xslx";
+            return GenerarExcel(nombreArchivo, transacciones);
+        }
+
         private FileResult GenerarExcel(string nombreArchivo,
             IEnumerable<Transaccion> transacciones)
         {
