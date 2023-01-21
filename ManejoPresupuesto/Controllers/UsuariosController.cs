f4ad316a8a6b025ef23b485d494586038736215a
Valeria Soledad Heredia <valeheredia97@gmail.com>
2023-01-20 21:48:59 -0300

    Sistema de usuarios: Formulario de registro. Configurando el sistema de usuarios

---
+++ ManejoPresupuesto/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroViewModel modelo)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+            return RedirectToAction("Index", "Transacciones");
+        }
+    }
+}
