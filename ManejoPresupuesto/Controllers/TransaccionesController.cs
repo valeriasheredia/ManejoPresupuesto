79af693f5e910b73f229c7e1c691e7ab14a86d6f
Valeria Soledad Heredia <valeheredia97@gmail.com>
2023-01-25 22:45:32 -0300

    Evento click en el calendario

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -260,6 +260,18 @@ namespace ManejoPresupuesto.Controllers
             return Json(eventosCalendario);
         }
 
+        public async Task<JsonResult> ObtenerTransaccionesPorFecha(DateTime fecha)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var transacciones = await repositorioTransacciones.ObtenerPorUsuarioId(
+             new ParametroObtenerTransaccionesPorUsuario
+             {
+                 UsuarioId = usuarioId,
+                 FechaInicio = fecha,
+                 FechaFin = fecha
+             });
+            return Json(transacciones);
+        }
         public async Task<IActionResult> Crear()
         {
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
